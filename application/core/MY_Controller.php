c9afef8f6eed34e37124360dad779790358536c9
itischarles <itissucex@yahoo.co.uk>
2016-03-29 13:54:31 +0100

    first push of all files

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,8 @@ class MY_Controller extends CI_Controller {
     
     function __construct() {
 	parent::__construct();
+	
+	date_default_timezone_set('Europe/London');
 
 	$this->user_accessor =  new User_model(); 
 	
