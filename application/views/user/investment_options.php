e05024a71147e9bd33fe58efa459286e65c1ae96
sys1 <sys1@sys1-desktop>
2016-04-27 15:34:00 +0530

    dashboard - application - investment_option  add function completed

--- application/views/user/investment_options.php
+++ application/views/user/investment_options.php
@@ -1,21 +1,29 @@
 <div class="">
     
     
+    
     <h1 class="text-center"><b></b>Investment Options</h1> 
     <hr style="display: block;margin-top: 0.5em;    margin-bottom: 0.5em;    margin-left: auto;
         margin-right: auto;    border-style: inset;    border-width: 2px;">
+    
+     <form  role="form" action='<?php echo base_url("investmentsave") ?>' method="post">
+      
+    <input type="hidden" name="investmentIntructionID" class="form-control"> 
+    <input type="hidden" name="applicationID" class="form-control" value="<?php echo @$app_id;?>"> 
+  
     <div class="col-sm-12">
+        
         <div class="row">
             <div class="col-sm-3"><h3>Investment Options <b style="color: red;">*</b></h3> </div>
             <div class="col-sm-9">
                 <div class="radio">
-                    <label><input type="radio" name="optradio">IM Optimum Growth</label>
+                    <label><input type="radio" name="investment_options" value="IM Optimum Growth" checked>IM Optimum Growth</label>
                 </div>
                 <div class="radio">
-                    <label><input type="radio" name="optradio">IM Optimum Income</label>
+                    <label><input type="radio" name="investment_options" value="IM Optimum Income">IM Optimum Income</label>
                 </div>
                 <div class="radio">
-                    <label><input type="radio" name="optradio">IM Optimum Growth & Income </label>
+                    <label><input type="radio" name="investment_options" value="IM Optimum Growth & Income">IM Optimum Growth & Income </label>
                 </div>
                 <p>Please select the investment portfolio you require(you can change this at any time)</p> </p>
             </div>
@@ -29,12 +37,7 @@
             <div class="row">
                 <div class="form-group  col-sm-3">
 
-                    <select class="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                    </select>
+                    <input type="number"  class="form-control" name="percentage_of_investment" min="1" max="10">
                 </div>
                 </div>
                 <p>For example 100% unless you selected more then one option (in which case you must ensure the total in each of these boxes amounts to 100%)</p>
@@ -47,19 +50,22 @@
             <div class="col-sm-3"><h3>What is your Target Date?</h3> </div>
             <div class="col-sm-9">
              <div class="row">
-                <div class="form-group col-sm-3">
-                <div class='input-group date' id='datetimepicker1'>
-                    <input type='text' class="form-control" />
+                <div class="form-group  col-sm-4">
+                  
+                    <label> <input id="target_dates" name="target_dates" value="Date"  class="form-control" ><span class="glyphicon glyphicon-calendar input-group-addon"></span></label>
+                  
+                 
+                </div>
+<!--                <div class="form-group col-sm-3">
+                <div class='input-group date' id='thedate'>
+                    <input type='text'  class="form-control" />
                     <span class="input-group-addon">
                         <span class="glyphicon glyphicon-calendar"></span>
                     </span>
                 </div>
-                  <script type="text/javascript">
-            $(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
-        </script>
-            </div>
+                  
+        
+            </div>-->
             </div>
                       
        
@@ -68,11 +74,11 @@
             
             </div>
          <div class="form-group col-sm-offset-3">  
-             <button type="button" class="btn btn-primary btn-lg">Save</button>
+             <button type="submit" class="btn btn-primary btn-lg">Save</button>
         </div>
             
 
         </div>
     </div>
-
+     </form> 
 </div>
